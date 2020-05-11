0ac26acf032e7bba5d291b63cafac59a23de2de5
Bozhek Yuliia <juli-lk@hotmail.com>
2020-05-11 17:07:22 +0300

    added ci configuration

--- src/config/index.js
+++ src/config/index.js
@@ -10,10 +10,10 @@ if (!envFound) {
 
 export default {
     port: parseInt(process.env.PORT, 10),
-    DB_string: process.env.MONGO_URI,
+    DB_string: process.env.MONGO_URI || "mongodb+srv://atlasMongoDBadmin:atlasMongoDBadmin@vigocluster-fywki.mongodb.net/VigoDB?retryWrites=true&w=majority",
     allowCors: process.env.ALLOW_CORS,
-    environment: process.env.NODE_ENV,
-    secret: process.env.SECRET_OR_KEY,
+    environment: process.env.NODE_ENV || "production",
+    secret: process.env.SECRET_OR_KEY || "radftghjnb vcgzafsygdq6r534r6768yriudghzjcbzjsk",
     //Signing a token with 1 hour of expiration by default on production
     expiresInMinutes: process.env.NODE_ENV === "development"
         ? 45000
