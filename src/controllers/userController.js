d03f5cc3fd6e4af15ac36c32ca16815d7653dd12
Bozhek Yuliia <juli-lk@hotmail.com>
2020-05-27 17:32:19 +0300

    users controller change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,13 +24,13 @@ export const createUser = (req, res, next) => {
             if (customer) {
                 if (customer.email === email) {
                     return res
-                        .status(200)
+                        .status(400)
                         .json({message: `Email ${customer.email} already exists"`});
                 }
 
                 if (customer.login === login) {
                     return res
-                        .status(200)
+                        .status(400)
                         .json({message: `Login ${customer.login} already exists`});
                 }
             }
