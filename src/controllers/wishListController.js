a3aac4da7326071532ed5a87176b771638d469a2
Bozhek Yuliia <juli-lk@hotmail.com>
2020-06-03 11:57:38 +0300

    wish list controller change

--- src/controllers/wishListController.js
+++ src/controllers/wishListController.js
@@ -3,22 +3,18 @@ import {log} from '../utils/helper';
 
 export const addProductToWishList = (req, res, next) => {
     const {productId, userId} = req.body;
-
     if (!productId) {
         res.status(400)
             .json({
                 message: `add product to wish list error: productId parameter is required" `
             });
     }
-
     if (!userId) {
         res.status(400)
             .json({
                 message: `add product to wish list error: userId parameter is required" `
             });
     }
-
-
     WishList.find({userId: userId})
         .then((userWishes) => {
             if (!userWishes.length) {
@@ -38,7 +34,7 @@ export const addProductToWishList = (req, res, next) => {
                     .catch(error => {
                             res.status(400)
                                 .json({
-                                    message: `New wish adding error: ${error}`
+                                    message: `New wish adding error: ${error.message}`
                                 });
                             log(error);
                             next(error);
@@ -46,23 +42,41 @@ export const addProductToWishList = (req, res, next) => {
                     );
             } else {
                 const wishes = userWishes[0];
-                wishes.products.push(productId);
-                wishes.save()
-                    .then(() => res
+                const products = wishes.products.map(pr => pr._id.toString());
+                if (products.includes(productId)) {
+                    res.status(200)
+                        .json({
+                            message: 'Success operation. The product is already in your wishList',
+                            wishes
+                        });
+                } else {
+                    products.push(productId);
+
+                    WishList.findOneAndUpdate(
+                        {userId: userId},
+                        {
+                            $set: {
+                                userId: userId,
+                                products: products
+                            }
+                        },
+                        {new: true, runValidators: true}
+                    ).then(() => res
                         .status(200)
                         .json({
                             message: 'Success operation. New wish is added'
                         })
                     )
-                    .catch(error => {
-                            res.status(400)
-                                .json({
-                                    message: `New wish adding error: ${error}`
-                                });
-                            log(error);
-                            next(error);
-                        }
-                    );
+                        .catch(error => {
+                                res.status(400)
+                                    .json({
+                                        message: `New wish adding error: ${error.message}`
+                                    });
+                                log(error);
+                                next(error);
+                            }
+                        );
+                }
             }
         })
         .catch(error => {
@@ -87,7 +101,7 @@ export const getAllWishListData = (req, res, next) => {
         .catch(error => {
                 res.status(400)
                     .json({
-                        message: `Getting wishes error: ${error}`
+                        message: `Getting wishes error: ${error.message}`
                     });
                 next(error);
             }
@@ -97,80 +111,75 @@ export const getAllWishListData = (req, res, next) => {
 export const getUserWishes = (req, res, next) => {
     const userId = req.params.id;
 
-    if (!userId) {
-        res.status(400)
-            .json({
-                message: `getting user wish list error: userId parameter is required" `
-            });
-    } else {
-        WishList.find({userId: userId})
-            .then(items => {
-                    return res.status(200)
-                        .send({items});
-                }
-            )
-            .catch(error => {
-                    res.status(400)
-                        .json({
-                            message: `Getting user wishes error: ${error}`
-                        });
-                    next(error);
-                }
-            );
-    }
-
-
+    WishList.find({userId: userId})
+        .then(userWishList => {
+            return res.status(200)
+                .send({userWishList});
+        })
+        .catch(error => {
+            res.status(400)
+                .json({
+                    message: `Getting user wishes error: ${error.message}`
+                });
+            next(error);
+        });
 };
 
 export const deleteProductFromWishlist = (req, res, next) => {
-    const productId = req.params.productId;
-    const userId = req.params.userId;
-    if (!productId) {
-        res.status(400)
-            .json({
-                message: `delete product from wish list error: productId parameter is required" `
-            });
-    }
+    const productId = req.params.id;
+    const userId = req.user.id;
 
-    if (!userId) {
-        res.status(400)
-            .json({
-                message: `delete product from wish list error: userId parameter is required" `
-            });
-    }
-
-
-    WishList.find({userIdd: userId})
+    WishList.find({userId: userId})
         .then((userWishes) => {
-            if (!userWishes) {
+            if (!userWishes.length) {
                 res.status(400)
                     .json({
                         message: `user has no wish list yet`
                     });
-            } else if (!userWishes.products.length) {
+            } else if (!userWishes[0].products.length) {
                 res.status(400)
                     .json({
                         message: `user ${userId} has no product ${productId} in wishList`
                     });
             } else {
-                const newProductList = userWishes.products.filter(product => product === productId);
-                userWishes.products = newProductList;
-                userWishes.save()
-                    .then(() => res
+                const wishes = userWishes[0];
+                const products = wishes.products.map(pr => pr._id.toString());
+                if (!products.includes(productId)) {
+                    res.status(400)
+                        .json({
+                            message: `user ${userId} has no product ${productId} in wishList`
+                        });
+                } else {
+
+                    const newProductList = wishes.products
+                        .filter(pr => pr._id.toString() !== productId)
+                        .map(pr => pr._id.toString());
+
+                    WishList.findOneAndUpdate(
+                        {userId: userId},
+                        {
+                            $set: {
+                                userId: userId,
+                                products: newProductList
+                            }
+                        },
+                        {new: true, runValidators: true}
+                    ).then(() => res
                         .status(200)
                         .json({
-                            message: 'Success operation. product is removed from wish list',
+                            message: 'Success operation. Product is deleted from wishlist'
                         })
                     )
-                    .catch(error => {
-                            res.status(400)
-                                .json({
-                                    message: `product remove from wish list error: ${error.message}`
-                                });
-                            log(error);
-                            next(error);
-                        }
-                    );
+                        .catch(error => {
+                                res.status(400)
+                                    .json({
+                                        message: `Wish list error: ${error.message}`
+                                    });
+                                log(error);
+                                next(error);
+                            }
+                        );
+                }
             }
         })
         .catch(error => {
