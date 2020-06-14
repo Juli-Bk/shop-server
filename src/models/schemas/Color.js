df9e961b24b7ce803d52286124f96ef52585b619
Bozhek Yuliia <juli-lk@hotmail.com>
2020-06-14 21:30:37 +0300

    colors hex change

--- src/models/schemas/Color.js
+++ src/models/schemas/Color.js
@@ -10,10 +10,10 @@ const baseColors = [
     {name: 'gold', hex: '#e3cf49'}, {name: 'purple', hex: '#e004e0'},
     {name: 'green', hex: '#08d408'}, {name: 'red', hex: '#ff0000'},
     {name: 'grey', hex: '#808080'}, {name: 'silver', hex: '#e3e2e2'},
-    {name: 'yellow', hex: '#ffff00'}, {name: 'white', hex: '#FFFFFF'},
+    {name: 'yellow', hex: '#ffff00'}, {name: 'white', hex: '#faf9f9'},
     {name: 'mocha', hex: '#d0c7b6'}, {name: 'mint', hex: '#c1f1d9'},
     {name: 'beige', hex: '#d7b68f'}, {name: 'brown', hex: '#653606'},
-    {name: 'multi', hex: '#ffffff'}, {name: 'animal', hex: '#ffffff'},
+    {name: 'multi', hex: '#faf9f9'}, {name: 'animal', hex: '#faf9f9'},
 ];
 const colorsNameList = baseColors.map(color => color.name.toLowerCase());
 const errorMsg = ` can be specified from this list: ${colorsNameList.join(', ')}`;
