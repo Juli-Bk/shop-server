4201861b607276db5510e200ff3ada409ff2f40f
Bozhek Yuliia <juli-lk@hotmail.com>
2020-06-25 14:02:32 +0300

    cookie change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -213,12 +213,12 @@ export const loginUser = (req, res, next) => {
                                             .cookie('refreshToken', savedRT.token, {
                                                 maxAge: savedRT.exp,
                                                 httpOnly: true,
-                                                sameSite: 'Lax',
+                                                sameSite: 'None',
                                                 secure: true
                                             })
                                             .cookie('token', token, {
                                                 maxAge: savedRT.exp,
-                                                sameSite: 'Lax',
+                                                sameSite: 'None',
                                                 secure: true
                                             })
                                             .json({
@@ -245,12 +245,12 @@ export const loginUser = (req, res, next) => {
                                                             .cookie('refreshToken', newRefreshToken, {
                                                                 maxAge: expiresRTInMS,
                                                                 httpOnly: true,
-                                                                sameSite: 'Lax',
+                                                                sameSite: 'None',
                                                                 secure: true
                                                             })
                                                             .cookie('token', token, {
                                                                 expires: expiresInMinutes,
-                                                                sameSite: 'Lax',
+                                                                sameSite: 'None',
                                                                 secure: true
                                                             })
                                                             .json({
@@ -296,12 +296,12 @@ export const loginUser = (req, res, next) => {
                                                 .cookie('refreshToken', newRefreshToken, {
                                                     maxAge: expiresRTInMS,
                                                     httpOnly: true,
-                                                    sameSite: 'Lax',
+                                                    sameSite: 'None',
                                                     secure: true
                                                 })
                                                 .cookie('token', token, {
                                                     expires: expiresInMinutes,
-                                                    sameSite: 'Lax',
+                                                    sameSite: 'None',
                                                     secure: true
                                                 })
                                                 .json({
@@ -379,12 +379,12 @@ export const refreshToken = (req, res, next) => {
             .cookie('refreshToken', refToken, {
                 maxAge: expDate,
                 httpOnly: true,
-                sameSite: 'Lax',
+                sameSite: 'None',
                 secure: true
             })
             .cookie('token', token, {
                 expires: expiresInMinutes,
-                sameSite: 'Lax',
+                sameSite: 'None',
                 secure: true
             })
             .json({
